09314973500a6f3b36ad80a5e8cbb13fac525922
dgrech <danielgrech91@gmail.com>
2021-11-10 06:24:23 +1100

    Fix CLI hanging after command execution

--- cli/src/main/kotlin/com/dgsd/ksol/cli/Main.kt
+++ cli/src/main/kotlin/com/dgsd/ksol/cli/Main.kt
@@ -3,11 +3,15 @@ package com.dgsd.ksol.cli
 import com.dgsd.ksol.cli.keygen.KeygenCommand
 import com.dgsd.ksol.cli.rpc.RpcCommand
 import com.github.ajalt.clikt.core.subcommands
+import kotlin.system.exitProcess
 
-fun main(arguments: Array<String>) =
+fun main(arguments: Array<String>) {
     CliCommand()
         .subcommands(
             RpcCommand.create(),
             KeygenCommand.create(),
         )
         .main(arguments)
+
+    exitProcess(0)
+}
--- lib/src/main/kotlin/com/dgsd/ksol/jsonrpc/networking/util/OkHttpExtensions.kt
+++ lib/src/main/kotlin/com/dgsd/ksol/jsonrpc/networking/util/OkHttpExtensions.kt
@@ -2,12 +2,32 @@ package com.dgsd.ksol.jsonrpc.networking.util
 
 import kotlinx.coroutines.suspendCancellableCoroutine
 import okhttp3.Call
+import okhttp3.Callback
 import okhttp3.Response
+import java.io.IOException
+import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
 
 internal suspend inline fun Call.await(): Response {
     return suspendCancellableCoroutine { continuation ->
-        val callback = OkHttpContinuationCallback(this, continuation)
-        enqueue(callback)
-        continuation.invokeOnCancellation(callback)
+        enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                if (!continuation.isCancelled) {
+                    continuation.resumeWithException(e)
+                }
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                continuation.resume(response)
+            }
+        })
+
+        continuation.invokeOnCancellation {
+            try {
+                cancel()
+            } catch (_: Throwable) {
+                // Ignored..
+            }
+        }
     }
 }
