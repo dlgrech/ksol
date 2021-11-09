7088824eb80d3bf82a75aeeef04ce5c5203339a2
dgrech <danielgrech91@gmail.com>
2021-11-10 06:42:00 +1100

    Introduce SystemProgram for creating TransactionInstructions

---
+++ lib/src/main/kotlin/com/dgsd/ksol/programs/SystemProgram.kt
@@ -0,0 +1,40 @@
+package com.dgsd.ksol.programs
+
+import com.dgsd.ksol.model.Lamports
+import com.dgsd.ksol.model.PublicKey
+import com.dgsd.ksol.model.TransactionInstruction
+import org.bitcoinj.core.Utils
+
+/**
+ * @see <a href="https://docs.solana.com/developing/runtime-facilities/programs#system-program#>System Instruction Enum</a>
+ */
+object SystemProgram {
+    private val PROGRAM_ID = PublicKey.fromBase58("11111111111111111111111111111111")
+
+    /**
+     * The index of the different instructions available in the SystemProgram
+     *
+     * @see <a href="https://docs.rs/solana-sdk/1.8.2/solana_sdk/system_instruction/enum.SystemInstruction.html#>System Instruction Enum</a>
+     */
+    private const val PROGRAM_INDEX_TRANSFER = 2
+
+    fun transfer(
+        sender: PublicKey,
+        recipient: PublicKey,
+        lamports: Lamports,
+    ): TransactionInstruction {
+        val spaceForInstructionIndex = 4
+        val spaceForLamports = 8
+
+        val inputData = ByteArray(spaceForInstructionIndex + spaceForLamports).apply {
+            Utils.uint32ToByteArrayLE(PROGRAM_INDEX_TRANSFER.toLong(), this, 0)
+            Utils.int64ToByteArrayLE(lamports, this, spaceForInstructionIndex)
+        }
+
+        return TransactionInstruction(
+            programAccount = PROGRAM_ID,
+            inputAccounts = listOf(sender, recipient),
+            inputData = inputData
+        )
+    }
+}
