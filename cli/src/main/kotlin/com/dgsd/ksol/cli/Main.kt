25fd0133f80bb68dc3b11d462fa31d950db503a5
dgrech <danielgrech91@gmail.com>
2021-11-10 13:59:42 +1100

    Make CLI handling more robust when child command fails

--- cli/src/main/kotlin/com/dgsd/ksol/cli/Main.kt
+++ cli/src/main/kotlin/com/dgsd/ksol/cli/Main.kt
@@ -3,15 +3,25 @@ package com.dgsd.ksol.cli
 import com.dgsd.ksol.cli.keygen.KeygenCommand
 import com.dgsd.ksol.cli.rpc.RpcCommand
 import com.github.ajalt.clikt.core.subcommands
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.supervisorScope
 import kotlin.system.exitProcess
 
 fun main(arguments: Array<String>) {
-    CliCommand()
-        .subcommands(
-            RpcCommand.create(),
-            KeygenCommand.create(),
-        )
-        .main(arguments)
+    runBlocking {
+        supervisorScope {
+            try {
+                CliCommand()
+                    .subcommands(
+                        RpcCommand.create(),
+                        KeygenCommand.create(),
+                    )
+                    .main(arguments)
+            } catch (ex: Exception) {
+                ex.printStackTrace()
+            }
+        }
+    }
 
     exitProcess(0)
 }
