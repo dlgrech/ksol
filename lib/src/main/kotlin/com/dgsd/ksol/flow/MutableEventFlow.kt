20fc55828a2a0f595abf42b3d08879f1f296c519
dgrech <danielgrech91@gmail.com>
2021-11-11 15:21:56 +1100

    Introduce MutableEventFlow, for emitting websocket updaates

---
+++ lib/src/main/kotlin/com/dgsd/ksol/flow/MutableEventFlow.kt
@@ -0,0 +1,22 @@
+package com.dgsd.ksol.flow
+
+import kotlinx.coroutines.channels.BufferOverflow
+import kotlinx.coroutines.flow.MutableSharedFlow
+
+/**
+ * `MutableSharedFlow` instance that can be used for emitting values as events.
+ *
+ * That is, they should only be handled [exactly once] by what ever subscriber is active at the time of emission.
+ */
+typealias MutableEventFlow<T> = MutableSharedFlow<T>
+
+/**
+ * Factory for creating [MutableEventFlow] instances
+ */
+fun <T> MutableEventFlow(): MutableEventFlow<T> {
+    return MutableSharedFlow(
+        replay = 0,
+        extraBufferCapacity = 1,
+        onBufferOverflow = BufferOverflow.SUSPEND
+    )
+}
