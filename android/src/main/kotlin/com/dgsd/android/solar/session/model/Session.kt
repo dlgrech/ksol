66948455fa18803a66eb485714f78bce047403e7
dgrech <danielgrech91@gmail.com>
2022-07-05 10:29:55 +1000

    Introduce Session model class

---
+++ android/src/main/kotlin/com/dgsd/android/solar/session/model/Session.kt
@@ -0,0 +1,53 @@
+package com.dgsd.android.solar.session.model
+
+import com.dgsd.ksol.model.KeyPair
+import com.dgsd.ksol.model.PrivateKey
+import com.dgsd.ksol.model.PublicKey
+
+/**
+ * Represents the authentication-level of the wallet being interacted with by the app
+ */
+sealed interface Session {
+
+    /**
+     * Used to uniquely identify the same wallet
+     */
+    val sessionId: String
+}
+
+/**
+ * [Session] when there is no wallet currently active
+ */
+object NoActiveWalletSession : Session {
+
+    override val sessionId = "no_active_wallet_session"
+}
+
+/**
+ * A [Session] that is tied to a particular account, as identified by [publicKey]
+ */
+sealed class WalletSession : Session {
+
+    /**
+     * The [PublicKey] associated with the wallet being interacted with by the app
+     */
+    abstract val publicKey: PublicKey
+
+    override val sessionId: String
+        get() = publicKey.toBase58String()
+}
+
+/**
+ * A [WalletSession] based on only a [PublicKey]. This is the [Session] used when a user has not
+ * given access to the underlying [PrivateKey] for the wallet
+ */
+data class PublicKeySession(override val publicKey: PublicKey) : WalletSession()
+
+/**
+ * A [WalletSession] with both a [PublicKey] and a [PrivateKey]. This is the [Session] used when a
+ * user has given access to the [PrivateKey] of the underlying wallet
+ */
+data class KeyPairSession(val keyPair: KeyPair) : WalletSession() {
+
+    override val publicKey = keyPair.publicKey
+}
