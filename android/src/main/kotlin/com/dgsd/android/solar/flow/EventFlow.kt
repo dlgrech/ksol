a3dcafd74a6766686309a2265566bd63b02692fd
dgrech <danielgrech91@gmail.com>
2022-07-07 10:35:10 +1000

    Introduce EventFlow

---
+++ android/src/main/kotlin/com/dgsd/android/solar/flow/EventFlow.kt
@@ -0,0 +1,19 @@
+package com.dgsd.android.solar.flow
+
+import kotlinx.coroutines.channels.BufferOverflow
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.SharedFlow
+
+typealias EventFlow<T> = SharedFlow<T>
+typealias MutableEventFlow<T> = MutableSharedFlow<T>
+
+fun <T> MutableEventFlow(): MutableEventFlow<T> {
+    return MutableSharedFlow(
+        replay = 1,
+        onBufferOverflow = BufferOverflow.DROP_OLDEST,
+    )
+}
+
+fun <T> MutableEventFlow<T>.asEventFlow(): EventFlow<T> {
+    return this
+}
