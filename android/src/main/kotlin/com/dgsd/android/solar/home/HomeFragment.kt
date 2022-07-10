62066cfda60b52e361673abfe6a5d32c6d3e1531
dgrech <danielgrech91@gmail.com>
2022-07-11 07:41:39 +1000

    Naive loading of transactions in home fragment

--- android/src/main/kotlin/com/dgsd/android/solar/home/HomeFragment.kt
+++ android/src/main/kotlin/com/dgsd/android/solar/home/HomeFragment.kt
@@ -4,13 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Text
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import com.dgsd.android.solar.common.util.collectAsStateLifecycleAware
@@ -27,25 +26,40 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?,
     ) = setContent {
 
-        val isLoading: Boolean by viewModel.isLoading.collectAsStateLifecycleAware(initial = false)
+        val isLoadingBalance: Boolean by viewModel.isLoadingBalance.collectAsStateLifecycleAware(initial = false)
         val balanceText by viewModel.balanceText.collectAsStateLifecycleAware(initial = null)
 
-        if (isLoading) {
-            Text(
-                text = "Loading!",
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .fillMaxHeight()
-                    .wrapContentSize(Alignment.Center)
-            )
-        } else {
-            Text(
-                text = "Balance: $balanceText",
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .fillMaxHeight()
-                    .wrapContentSize(Alignment.Center)
-            )
+        val isLoadingTransactions: Boolean by viewModel.isLoadingTransactionSignatures.collectAsStateLifecycleAware(initial = false)
+        val transactions by viewModel.transactions.collectAsStateLifecycleAware(initial = null)
+
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight()
+                .wrapContentSize(Alignment.Center)
+        ) {
+            if (isLoadingBalance) {
+                Text(text = "Loading Balance")
+            } else {
+                Text(text = "Balance: $balanceText")
+            }
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            if (isLoadingTransactions) {
+                Text(text = "Loading Transactions")
+            } else {
+                Column {
+                    Text(text = "Signatures:")
+                    if (transactions.isNullOrEmpty()) {
+                        Text(text = "None")
+                    } else {
+                        for (signature in transactions.orEmpty()) {
+                            Text(text = signature.toString())
+                        }
+                    }
+                }
+            }
         }
     }
 
--- android/src/main/kotlin/com/dgsd/android/solar/home/HomeViewModel.kt
+++ android/src/main/kotlin/com/dgsd/android/solar/home/HomeViewModel.kt
@@ -6,7 +6,10 @@ import com.dgsd.android.solar.common.util.ResourceFlowConsumer
 import com.dgsd.android.solar.session.model.WalletSession
 import com.dgsd.ksol.SolanaApi
 import com.dgsd.ksol.model.Lamports
+import com.dgsd.ksol.model.Transaction
 import kotlinx.coroutines.flow.map
+
+private const val NUM_TRANSACTIONS_TO_DISPLAY = 5
 
 class HomeViewModel(
     private val solanaApi: SolanaApi,
@@ -14,14 +17,25 @@ class HomeViewModel(
 ) : ViewModel() {
 
     private val balanceResourceConsumer = ResourceFlowConsumer<Lamports>(viewModelScope)
-
-    val isLoading = balanceResourceConsumer.isLoading
-
+    val isLoadingBalance = balanceResourceConsumer.isLoading
     val balanceText = balanceResourceConsumer.data.map { "$it Lamports" }
+
+    private val transactionsResourceConsumer = ResourceFlowConsumer<List<Transaction>>(viewModelScope)
+    val isLoadingTransactionSignatures = balanceResourceConsumer.isLoading
+    val transactions = transactionsResourceConsumer.data
 
     fun onCreate() {
         balanceResourceConsumer.collectFlow {
             solanaApi.getBalance(currentSession.publicKey)
+        }
+
+        transactionsResourceConsumer.collectFlow {
+            solanaApi.getSignaturesForAddress(
+                accountKey = currentSession.publicKey,
+                limit = NUM_TRANSACTIONS_TO_DISPLAY
+            ).map {
+                checkNotNull(solanaApi.getTransaction(it.signature))
+            }
         }
     }
 }
