edc284b5525da66555b61f17cae9020ab431fa6c
dgrech <danielgrech91@gmail.com>
2022-07-16 14:04:06 +1000

    Fix up CLI command to get private key

--- cli/src/main/kotlin/com/dgsd/ksol/cli/keygen/GenerateSeedCommand.kt
+++ cli/src/main/kotlin/com/dgsd/ksol/cli/keygen/GenerateSeedCommand.kt
@@ -5,15 +5,19 @@ import com.github.ajalt.clikt.core.CliktCommand
 import kotlinx.coroutines.runBlocking
 
 class GenerateSeedCommand : CliktCommand(
-    name = "seed",
-    help = "Generate key pair from mnemonic"
+  name = "seed",
+  help = "Generate key pair from mnemonic"
 ) {
 
-    private val passPhase by passPhraseOption()
-    private val mnemonic by mnemonicArgument()
+  private val passPhase by passPhraseOption()
+  private val mnemonic by mnemonicArgument()
 
-    override fun run() = runBlocking {
-        val seed = KeyFactory.createSeedFromMnemonic(mnemonic, passPhase)
-        echo(KeyFactory.createKeyPairFromSeed(seed))
-    }
+  override fun run() = runBlocking {
+    val seed = KeyFactory.createSeedFromMnemonic(mnemonic, passPhase)
+    val keyPair = KeyFactory.createKeyPairFromSeed(seed)
+    echo(
+      "publicKey: ${keyPair.publicKey.toBase58String()} " +
+          "/ privateKey: ${keyPair.privateKey.toBase58String()}"
+    )
+  }
 }
