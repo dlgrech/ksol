90882ccfa6e5f5ff8446e5b7d1109c3e01da4844
dgrech <danielgrech91@gmail.com>
2022-07-22 12:00:14 +1000

    Remove compose from repo

--- android/src/main/kotlin/com/dgsd/android/solar/home/HomeFragment.kt
+++ android/src/main/kotlin/com/dgsd/android/solar/home/HomeFragment.kt
@@ -1,67 +1,14 @@
 package com.dgsd.android.solar.home
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import androidx.compose.foundation.layout.*
-import androidx.compose.material3.Text
-import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
-import com.dgsd.android.solar.common.util.collectAsStateLifecycleAware
-import com.dgsd.android.solar.extensions.setContent
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class HomeFragment : Fragment() {
 
     private val viewModel: HomeViewModel by viewModel()
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?,
-    ) = setContent {
-
-        val isLoadingBalance: Boolean by viewModel.isLoadingBalance.collectAsStateLifecycleAware(initial = false)
-        val balanceText by viewModel.balanceText.collectAsStateLifecycleAware(initial = null)
-
-        val isLoadingTransactions: Boolean by viewModel.isLoadingTransactionSignatures.collectAsStateLifecycleAware(initial = false)
-        val transactions by viewModel.transactions.collectAsStateLifecycleAware(initial = null)
-
-        Column(
-            modifier = Modifier
-                .fillMaxWidth()
-                .fillMaxHeight()
-                .wrapContentSize(Alignment.Center)
-        ) {
-            if (isLoadingBalance) {
-                Text(text = "Loading Balance")
-            } else {
-                Text(text = "Balance: $balanceText")
-            }
-
-            Spacer(modifier = Modifier.height(16.dp))
-
-            if (isLoadingTransactions) {
-                Text(text = "Loading Transactions")
-            } else {
-                Column {
-                    Text(text = "Transactions:")
-                    if (transactions.isNullOrEmpty()) {
-                        Text(text = "None")
-                    } else {
-                        for (transaction in transactions.orEmpty()) {
-                            Text(text = transaction.toString())
-                        }
-                    }
-                }
-            }
-        }
-    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
