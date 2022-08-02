478d784ed485853a97fa5a882cec332155379f7b
dgrech <danielgrech91@gmail.com>
2022-08-02 10:54:00 +1000

    Introduce SolPay interface for interacting with SolPay elements

---
+++ solpay/src/main/kotlin/com/dgsd/ksol/solpay/SolPay.kt
@@ -0,0 +1,62 @@
+package com.dgsd.ksol.solpay
+
+import com.dgsd.ksol.SolanaApi
+import com.dgsd.ksol.model.Cluster
+import com.dgsd.ksol.model.PublicKey
+import com.dgsd.ksol.solpay.model.*
+import okhttp3.OkHttpClient
+
+/**
+ * Creates a default implementation of [SolPay] using the given [Cluster]
+ */
+fun SolPay(
+  cluster: Cluster,
+  okHttpClient: OkHttpClient = OkHttpClient(),
+): SolPay {
+  return SolPayImpl(okHttpClient, SolanaApi(cluster, okHttpClient))
+}
+
+/**
+ * API for performing SolPay-specific operations, as defined in the
+ * [SolPay spec](https://github.com/solana-labs/solana-pay/blob/master/SPEC.md)
+ */
+interface SolPay {
+
+  /**
+   * Construct a SolPay-compatible URL from the given transfer request
+   *
+   * This URL can be used to share with other SolPay compatible systems
+   */
+  fun createUrl(request: SolPayTransferRequest): String
+
+  /**
+   * Construct a SolPay-compatible URL from the given transaction request
+   *
+   * This URL can be used to share with other SolPay compatible systems
+   */
+  fun createUrl(request: SolPayTransactionRequest): String
+
+  /**
+   * Parses the given URL and return a SolPay request object, if applicable.
+   *
+   * @see [SolPayUrlParsingResult]
+   */
+  fun parseUrl(url: String): SolPayUrlParsingResult
+
+  /**
+   * Fetch the transaction request details for the SolPay transaction request
+   *
+   * @see https://github.com/solana-labs/solana-pay/blob/master/SPEC.md#get-request
+   */
+  suspend fun getDetails(request: SolPayTransactionRequest): SolPayTransactionRequestDetails
+
+  /**
+   * Fetches the actual transaction details to be performed as part of a SolPay transaction request
+   *
+   * @see https://github.com/solana-labs/solana-pay/blob/master/SPEC.md#post-request
+   */
+  suspend fun getTransaction(
+    signingWalletAddress: PublicKey,
+    request: SolPayTransactionRequest
+  ): SolPayTransactionInfo
+}
---
+++ solpay/src/main/kotlin/com/dgsd/ksol/solpay/SolPayImpl.kt
@@ -0,0 +1,148 @@
+package com.dgsd.ksol.solpay
+
+import com.dgsd.ksol.SolanaApi
+import com.dgsd.ksol.model.PublicKey
+import com.dgsd.ksol.solpay.api.TransactionRequestGetDetailsResponse
+import com.dgsd.ksol.solpay.api.TransactionRequestTransactionDetailsRequest
+import com.dgsd.ksol.solpay.api.TransactionRequestTransactionDetailsResponse
+import com.dgsd.ksol.solpay.extensions.await
+import com.dgsd.ksol.solpay.factory.*
+import com.dgsd.ksol.solpay.model.*
+import com.squareup.moshi.Moshi
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.RequestBody.Companion.toRequestBody
+
+private val MEDIA_TYPE_JSON = "application/json; charset=utf-8".toMediaType()
+
+/**
+ * Internal implementation of [SolPay] interface
+ */
+internal class SolPayImpl(
+  private val okHttpClient: OkHttpClient,
+  private val solanaApi: SolanaApi,
+) : SolPay {
+
+  private val moshiJson: Moshi = Moshi.Builder().build()
+
+  private val transactionRequestDetailsJsonAdapter =
+    moshiJson.adapter(TransactionRequestGetDetailsResponse::class.java)
+  private val transactionDetailsJsonAdapter =
+    moshiJson.adapter(TransactionRequestTransactionDetailsResponse::class.java)
+  private val transactionDetailsRequestJsonAdapter =
+    moshiJson.adapter(TransactionRequestTransactionDetailsRequest::class.java)
+
+  override fun createUrl(request: SolPayTransferRequest): String {
+    return SolPayTransferRequestFactory.createUrl(request)
+  }
+
+  override fun createUrl(request: SolPayTransactionRequest): String {
+    return SolPayTransactionRequestFactory.createUrl(request)
+  }
+
+  override fun parseUrl(url: String): SolPayUrlParsingResult {
+    val parsingResult = if (url.isTransactionRequestUrl()) {
+      runCatching {
+        SolPayUrlParsingResult.TransactionRequest(
+          SolPayTransactionRequestFactory.createRequest(url)
+        )
+      }
+    } else {
+      runCatching {
+        SolPayUrlParsingResult.TransferRequest(
+          SolPayTransferRequestFactory.createRequest(url)
+        )
+      }
+    }
+
+    return parsingResult.getOrElse { SolPayUrlParsingResult.ParsingError(it) }
+  }
+
+  override suspend fun getDetails(
+    request: SolPayTransactionRequest
+  ): SolPayTransactionRequestDetails {
+    val httpRequest = Request.Builder()
+      .url(request.link)
+      .get()
+      .build()
+    val httpResponse = okHttpClient.newCall(httpRequest).await()
+    val json = checkNotNull(httpResponse.body).string()
+    val detailsResponse = checkNotNull(transactionRequestDetailsJsonAdapter.fromJson(json))
+
+    return TransactionRequestDetailsFactory.create(detailsResponse)
+  }
+
+  override suspend fun getTransaction(
+    signingWalletAddress: PublicKey,
+    request: SolPayTransactionRequest
+  ): SolPayTransactionInfo {
+    val requestBody = TransactionRequestTransactionDetailsRequest(
+      account = signingWalletAddress.toBase58String()
+    )
+    val httpRequest = Request.Builder()
+      .url(request.link)
+      .post(transactionDetailsRequestJsonAdapter.toJson(requestBody).toRequestBody(MEDIA_TYPE_JSON))
+      .build()
+
+    val httpResponse = okHttpClient.newCall(httpRequest).await()
+    val json = checkNotNull(httpResponse.body).string()
+
+    val response = checkNotNull(transactionDetailsJsonAdapter.fromJson(json))
+
+    val transactionInfo =
+      TransactionRequestTransactionInfoFactory.create(signingWalletAddress, response)
+
+    return when {
+      transactionInfo.transaction.signatures.isEmpty() -> {
+        transactionInfo.withUpdatedBlockhash().withUpdatedFeePayer(signingWalletAddress)
+      }
+      transactionInfo.hasSingleSigner(signingWalletAddress) -> {
+        // The only required signature is for this account, update the recent blockhash
+        transactionInfo.withUpdatedBlockhash()
+      }
+      else -> transactionInfo
+    }
+  }
+
+  private suspend fun SolPayTransactionInfo.withUpdatedBlockhash(): SolPayTransactionInfo {
+    return copy(
+      transaction = transaction.copy(
+        message = transaction.message.copy(
+          recentBlockhash = PublicKey.fromBase58(solanaApi.getRecentBlockhash().blockhash)
+        )
+      )
+    )
+  }
+
+  private fun SolPayTransactionInfo.withUpdatedFeePayer(
+    signingWalletAddress: PublicKey
+  ): SolPayTransactionInfo {
+    // Set fee payer to the signing wallet
+    val newAccountKeys = transaction.message.accountKeys.map { accountKey ->
+      val isFeePayer = accountKey.publicKey == signingWalletAddress
+      accountKey.copy(
+        isFeePayer = isFeePayer,
+        isWritable = isFeePayer || accountKey.isWritable,
+        isSigner = isFeePayer || accountKey.isSigner
+      )
+    }.sortedBy {
+      if (it.isFeePayer) -1 else 0
+    }
+
+    return copy(
+      transaction = transaction.copy(
+        message = transaction.message.copy(accountKeys = newAccountKeys)
+      )
+    )
+  }
+
+  private fun SolPayTransactionInfo.hasSingleSigner(signer: PublicKey): Boolean {
+    return transaction.signatures.size == 1 &&
+      transaction.message.accountKeys[0].publicKey == signer
+  }
+
+  private fun String.isTransactionRequestUrl(): Boolean {
+    return startsWith(SolPayConstants.SCHEME_SOLANA + ":https")
+  }
+}
